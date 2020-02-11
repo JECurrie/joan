e252def52f8f8263615a45951833e589d5f5c873
Joan Currie <jecurrie8@gmail.com>
2020-02-11 02:09:17 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'static_pages/home'
   get 'static_pages/project'
   get 'static_pages/resume'
   get 'static_pages/contact'
-  root 'application#hello'
 end
